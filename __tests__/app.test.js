ccdc68642fb410a9a869ff97408aea6d2267b75b
Joseph Davis <joethebartenderpdx@gmail.com>
2021-05-06 17:21:45 -0700

    struggled with tests, forgot to commit

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,22 +7,32 @@ const request = supertest(app);
 
 describe('API Routes', () => {
 
-  // beforeAll(() => {
-  //   execSync('npm run setup-db');
-  // });
-
   afterAll(async () => {
     return client.end();
   });
 
+  describe('/api/sauces', () => {
 
+    let user;
 
-  beforeAll(() => {
-    execSync('npm run recreate-tables');
-  });
+    beforeAll(async () => {
+      execSync('npm run recreate-tables');
 
-  let aardvark =
-    {
+      const response = await request
+        .post('/api/auth/signup')
+        .send({
+          name: 'This is Name',
+          email: 'email@email.com',
+          password: 'password'
+        });
+
+      expect(response.status).toBe(200);
+
+      user = response.body;
+
+    });
+ 
+    let aardvark = {
       id: expect.any(Number),
       name: 'Aardvark',
       location: 'Oregon',
@@ -30,7 +40,7 @@ describe('API Routes', () => {
       img: 'sauces/aardvark.jpg'
     };
 
-  let apocalypse = 
+    let apocalypse = 
     {
       id: expect.any(Number),
       name: 'Apocalypse',
@@ -39,7 +49,7 @@ describe('API Routes', () => {
       img: 'sauces/zombie.jpg'
     };
 
-  let yellowBird = 
+    let yellowBird = 
     {
       id: expect.any(Number),
       name: 'Yellow Bird',
@@ -48,87 +58,106 @@ describe('API Routes', () => {
       img: 'sauces/yellowbird.jpg'
     };
 
-  it('POST aardvark to sauces', async () => {
-    // act - make the request
-    const response = await request
-      .post('/api/sauces')
-      .send(aardvark);
+    it('POST aardvark to sauces', async () => {
+      aardvark.userId = user.id;
+      // act - make the request
+      const response = await request
+        .post('/api/sauces')
+        .send(aardvark);
 
-    // was response OK (200)?
-    expect(response.status).toBe(200);
+      // was response OK (200)?
+      expect(response.status).toBe(200);
 
-    // did it return the data we expected?
-    expect(response.body).toEqual(aardvark);
+      // did it return the data we expected?
+      expect(response.body).toEqual(aardvark);
     
-    aardvark = response.body;
+      aardvark = response.body;
 
-  });
+    });
 
-  it('PUT updated aardvark to sauces', async () => {
+    it('PUT updated aardvark to sauces', async () => {
 
-    aardvark.location = 'Not Oregon';
-    aardvark.scoville = '1';
-    // act - make the request
-    const response = await request
-      .put(`/api/sauces/${aardvark.id}`)
-      .send(aardvark);
+      aardvark.location = 'Not Oregon';
+      aardvark.scoville = '1';
+      // act - make the request
+      const response = await request
+        .put(`/api/sauces/${aardvark.id}`)
+        .send(aardvark);
 
-    // was response OK (200)?
-    expect(response.status).toBe(200);
+      // was response OK (200)?
+      expect(response.status).toBe(200);
 
-    // did it return the data we expected?
-    expect(response.body).toEqual(aardvark);
+      // did it return the data we expected?
+      expect(response.body).toEqual(aardvark);
 
-  });
+    });
 
-  it('GET multiple sauces from sauces', async () => {
-    const sauce1 = await request.post('/api/sauces').send(apocalypse);
-    apocalypse = sauce1.body;
-    const sauce2 = await request.post('/api/sauces').send(yellowBird);
-    yellowBird = sauce2.body;
+    it('GET multiple sauces from sauces', async () => {
+      apocalypse.userId = user.id;
+      const sauce1 = await request.post('/api/sauces').send(apocalypse);
+      apocalypse = sauce1.body;
 
-    const response = await request.get('/api/sauces');
+      yellowBird.userId = user.id;
+      const sauce2 = await request.post('/api/sauces').send(yellowBird);
+      yellowBird = sauce2.body;
 
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual(expect.arrayContaining([aardvark, apocalypse, yellowBird]));
-  });
+      const response = await request.get('/api/sauces');
 
-  it('DELETE yellowBird from sauces', async () => {
-    const response = await request.get(`/api/sauces/${yellowBird.id}`);
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual(yellowBird);
+      expect(response.status).toBe(200);
+      
+      const expected = [aardvark, apocalypse, yellowBird].map(sauce => {
+        return {
+          userName: user.name,
+          ...sauce
+        };
+      });
+
+      expect(response.body).toEqual(expect.arrayContaining(expected));
+    });
+
+    it('DELETE yellowBird from sauces', async () => {
+      const response = await request.delete(`/api/sauces/${yellowBird.id}`);
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(yellowBird);
+
+      const getResponse = await request.get('/api/sauces');
+      expect(getResponse.status).toBe(200);
+      expect(getResponse.body.find(sauce => sauce.id === yellowBird.id)).toBeUndefined;
 
     
-  });
+    });
 
-  it('GET apocalypse from sauces', async () => {
-    const response = await request.get(`/api/sauces/${apocalypse.id}`);
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual(apocalypse);
+    it('GET apocalypse from sauces', async () => {
+      const response = await request.get(`/api/sauces/${apocalypse.id}`);
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({ ...apocalypse, userName: user.name });
+    });
+
+
+
+    // If a GET request is made to /api/sauces, does:
+    // 1) the server respond with status of 200
+    // 2) the body match the expected API data?
+    it.skip('GET /api/sauces', async () => {
+      // act - make the request
+      const response = await request.get('/api/sauces');
+
+      // was response OK (200)?
+      expect(response.status).toBe(200);
+
+      // did it return the data we expected?
+      expect(response.body).toEqual(expectedSauces);
+
+
+    });
+
+    // If a GET request is made to /api/sauces/:id, does:
+    // 1) the server respond with status of 200
+    // 2) the body match the expected API data for the cat with that id?
+    it.skip('GET /api/sauces/:id', async () => {
+      const response = await request.get('/api/sauces/2');
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(expectedSauces[1]);
+    });
   });
 });
-
-// If a GET request is made to /api/sauces, does:
-// 1) the server respond with status of 200
-// 2) the body match the expected API data?
-// it.skip('GET /api/sauces', async () => {
-//   // act - make the request
-//   const response = await request.get('/api/sauces');
-
-//   // was response OK (200)?
-//   expect(response.status).toBe(200);
-
-//   // did it return the data we expected?
-//   expect(response.body).toEqual(expectedSauces);
-
-// });
-
-// If a GET request is made to /api/sauces/:id, does:
-// 1) the server respond with status of 200
-// 2) the body match the expected API data for the cat with that id?
-//   it.skip('GET /api/sauces/:id', async () => {
-//     const response = await request.get('/api/sauces/2');
-//     expect(response.status).toBe(200);
-//     expect(response.body).toEqual(expectedSauces[1]);
-//   });
-// });
